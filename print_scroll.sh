54bfe0f6967d3dc696af00398f5c2fd057cfd9d1
Petrie <peter.rogers@gmail.com>
2026-08-06 17:15:42 -0400

    feat: CLI args for process script

--- print_scroll.sh
+++ print_scroll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./process.py $* || exit
-convert out.png -resample 18 -density 72 -resize "162x" out.ps
+./process.py --width 640 --margin 30 --gutter 15 --dest out.png $* || exit
+convert out.png -resample 36 -density 72 -resize "162x" out.ps
 lp -d Epson-TM-T88V -o TmtPaperSource=DocFeedNoCut -o TmtPaperReduction=Both out.ps
--- process.py
+++ process.py
@@ -1,38 +1,94 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import sys
 import PIL, PIL.Image, PIL.ImageDraw
 
 
-GAP = 10
-MARGIN = 25
+DEFAULT_WIDTH = 320
+DEFAULT_GUTTER = 10
+DEFAULT_MARGIN = 25
 
 
-files = sys.argv[1:]
-assert files
+def make_strip(images, margin, gutter, width):
+    images = resize_images(images, width)
+    total_width = width + 2*margin
+    total_height = sum([
+        img.size[1]
+        for img in images
+    ]) + 2*margin + (len(images)-1) * gutter
 
-images = []
-for src in files:
-    img = PIL.Image.open(src)
-    images.append(img)
+    output_img = PIL.Image.new('RGB', (total_width, total_height))
+    x = margin
+    y = margin
+    for img in images:
+        output_img.paste(img, (x, y))
+        y += img.height + gutter
+    return output_img
 
 
-total_width = max([
-    img.size[0]
-    for img in images
-]) + 2*MARGIN
-total_height = sum([
-    img.size[1]
-    for img in images
-]) + 2*MARGIN + (len(images)-1) * GAP
+def resize_images(images, width):
+    def _get_height(img):
+        return int((width/img.width)*img.height)
+    return [
+        img.resize((width, _get_height(img)))
+        for img in images
+    ]
 
-output_img = PIL.Image.new('RGB', (total_width, total_height))
 
-x = MARGIN
-y = MARGIN
-for img in images:
-    output_img.paste(img, (x, y))
-    y += img.size[1] + GAP
+def main():
+    parser = argparse.ArgumentParser(
+        description='Creates a vertical strip of photos',
+    )
+    parser.add_argument(
+        '--dest',
+        nargs=1,
+        help='The output image',
+    )
+    parser.add_argument(
+        'src',
+        nargs='+',
+        help='The input images',
+    )
+    parser.add_argument(
+        '--margin',
+        type=int,
+        default=DEFAULT_MARGIN,
+        help='The page margin (pixels)',
+    )
+    parser.add_argument(
+        '--gutter',
+        type=int,
+        default=DEFAULT_GUTTER,
+        help='The space between images (pixels)',
+    )
+    parser.add_argument(
+        '--width',
+        type=int,
+        default=DEFAULT_WIDTH,
+        help='The target width of the output image (pixels)',
+    )
+    args = parser.parse_args(sys.argv[1:])
+    files = args.src
+    dest = args.dest[0]
+    margin = args.margin
+    gutter = args.gutter
+    width = args.width
 
-output_img.save('out.png')
+    images = []
+    for src in files:
+        img = PIL.Image.open(src)
+        images.append(img)
+
+    output = make_strip(
+        images,
+        margin=margin,
+        gutter=gutter,
+        width=width,
+    )
+    output.save(dest)
+
+
+if __name__ == '__main__':
+    main()
