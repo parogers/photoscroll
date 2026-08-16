18e031d0e3fa210a0717d4a2c35aa2e5c64fa6b0
Petrie <peter.rogers@gmail.com>
2026-08-16 19:22:19 -0400

    feat: WIP server-side job processing

---
+++ server/job_manager.py
@@ -0,0 +1,34 @@
+
+import time
+import os
+import aiofiles
+from asyncio import Queue
+from fastapi import FastAPI, File, UploadFile
+
+
+async def stream_file(infile, dest):
+    async with aiofiles.open(dest, 'wb') as outfile:
+        while chunk := await infile.read(100*1024):
+            await outfile.write(chunk)
+
+
+class JobManager:
+    def __init__(self):
+        self.queue = Queue()
+        self.job_dir = './jobs'
+        self.job_id = int(time.time())
+        if not os.path.exists(self.job_dir):
+            os.mkdir(self.job_dir)
+
+    def get_next_job_id(self):
+        next_id = self.job_id
+        self.job_id += 1
+        return next_id
+
+    async def submit(self, files: list[UploadFile]):
+        base_dir = os.path.join(self.job_dir, str(self.get_next_job_id()))
+        os.mkdir(base_dir)
+        for n, file in enumerate(files):
+            path = os.path.join(base_dir, f'photo{n:04d}.png')
+            await stream_file(file, path)
+        await self.queue.put(base_dir)
--- server/main.py
+++ server/main.py
@@ -1,11 +1,40 @@
 
+import os
+import asyncio
+from contextlib import asynccontextmanager
 import subprocess
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from job_manager import JobManager
+
+
+async def worker(queue):
+    print('starting worker...')
+    while True:
+        job_dir = await queue.get()
+        try:
+            images = [
+                os.path.join(job_dir, fname)
+                for fname in os.listdir(job_dir)
+            ]
+            subprocess.run([
+                './print_scroll.sh',
+            ] + images)
+        except Exception as exc:
+            print('worker exception', exc)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    task = asyncio.create_task(worker(job_manager.queue))
+    yield
+    task.cancel()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,17 +44,14 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+job_manager = JobManager()
+
 @app.get('/')
 async def index():
     return ''
 
 
 @app.post('/upload')
-async def hello(file: UploadFile):
-    with open('out.png', 'wb') as outfile:
-        outfile.write(await file.read())
-    subprocess.run([
-        './print_scroll.sh',
-        'out.png',
-    ])
+async def hello(files: list[UploadFile]):
+    await job_manager.submit(files)
     return ''
