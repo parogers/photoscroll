9395772378f983e8d0072f25a0e083d9c6bb9300
Petrie <peter.rogers@gmail.com>
2026-08-27 15:32:37 -0400

    fix: pause camera feed when capturing

--- client/src/App.vue
+++ client/src/App.vue
@@ -32,9 +32,11 @@ onMounted(() => {
 async function onCapture(ev: any) {
     ev.preventDefault();
     capturing.value = true;
+    videoEl.value!.pause();
     await takePicture();
     await nextTick();
     await new Promise(resolve => setTimeout(resolve, 0));
+    videoEl.value!.play();
     capturing.value = false;
 }
 
